6dd5217a73fcf844865711472871a7a7d0ccb669
sbhatm1213 <sbhatm1213@gmail.com>
2023-09-12 23:38:24 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -2,10 +2,21 @@
 
 def binary_search(searchlist, numtosearch):
 
+  sorted_searchlist = sorted(searchlist)
+  
   first = 0
   last = len(searchlist)-1
 
-  print(midpoint = (last - first)/2)
+
+  while first < last:
+    midpoint = int((last - first)/2)
+    if numtosearch < sorted_searchlist[midpoint]:
+      last = midpoint
+    elif numtosearch > sorted_searchlist[midpoint]:
+      first = midpoint
+    elif numtosearch == sorted_searchlist[midpoint]:
+      return midpoint
+  return None
     
 numstotest = [1,2,3,4,5,6,7,8,9]
 binary_search(numstotest, 12)
