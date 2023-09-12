cbbb8925c1cf19359829c873b1cfcd7b305951c3
sbhatm1213 <sbhatm1213@gmail.com>
2023-09-12 22:39:54 +0530

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,12 @@
+
+
+def binary_search(searchlist, numtosearch):
+
+  first = 0
+  last = len(searchlist)-1
+
+  print(midpoint = (last - first)/2)
+    
+numstotest = [1,2,3,4,5,6,7,8,9]
+binary_search(numstotest, 12)
+binary_search(numstotest, 6)
