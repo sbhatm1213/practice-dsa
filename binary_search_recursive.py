54bcdf1ba1417921d7d07c7796f6e9db2c9b7d87
sbhatm1213 <sbhatm1213@gmail.com>
2023-09-27 00:18:24 +0530

    Update binary_search_recursive.py

--- binary_search_recursive.py
+++ binary_search_recursive.py
@@ -16,8 +16,27 @@ def binary_search_recursive(sortedlist, x):
     return "Found "
 
 
+def recursive_binary_search(list, target, start=0, end=None):
+    if end is None:
+        end = len(list) - 1
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+
+    if target == list[mid]:
+        return mid
+    else:
+        if target < list[mid]:
+            return recursive_binary_search(list, target, start, mid-1)
+        else:
+            return recursive_binary_search(list, target, mid+1, end)
+
 numstotest = [1,2,3,4,5,6,7,8,9]
 print(binary_search_recursive(numstotest, 12))
 print(binary_search_recursive(numstotest, 6))  
-  
+print("===========================================")
+print(recursive_binary_search(numstotest, 12))
+print(recursive_binary_search(numstotest, 6))
+
   
